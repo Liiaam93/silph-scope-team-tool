01e208158843e1e6014f0c9ac0cecbc71a790110
Liiaam93 <81435487+Liiaam93@users.noreply.github.com>
2021-12-21 12:01:38 +0000

    jimmy

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,41 +1,49 @@
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeProvider } from "@chakra-ui/react";
 import { RecoilRoot } from "recoil";
 import NextNProgress from "nextjs-progressbar";
 import Head from "next/head";
+import customTheme from "./../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Head>
-        <title>YEEEEET 2.0</title>
-        <meta name="tool of tools" content="Made by Liiiaaam93" />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/apple-touch-ico.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favico-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favico-16x16.png"
-        />
-        <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
-        <meta name="msapplication-TileColor" content="#da532c" />
-        <meta name="theme-color" content="#ffffff"></meta>
-      </Head>
-      <RecoilRoot>
-        <NextNProgress height={5} />
-        <Component {...pageProps} />
-      </RecoilRoot>
+    <ChakraProvider resetCSS theme={customTheme}>
+      <ColorModeProvider
+        options={{
+          initialColorMode: "light",
+          useSystemColorMode: true,
+        }}
+      >
+        <Head>
+          <title>YEEEEET 2.0</title>
+          <meta name="tool of tools" content="Made by Liiiaaam93" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-ico.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favico-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favico-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff"></meta>
+        </Head>
+        <RecoilRoot>
+          <NextNProgress height={5} />
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </ColorModeProvider>
     </ChakraProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,21 @@
 import type { NextPage } from "next";
 import Navbar from "../components/Navbar";
 import Changelog from "../components/Changelog";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
     <>
       <Navbar />
-      <Changelog />
+      <Flex
+        pt="15vh"
+        minHeight="100vh"
+        flexDir="column"
+        alignContent="center"
+        bgColor="#414141"
+      >
+        <Changelog />
+      </Flex>
     </>
   );
 };
